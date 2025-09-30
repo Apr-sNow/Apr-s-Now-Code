5a058a424ed0908dd506950c06cc4b5ad08e7144
孟夏雪 <2953904416@qq.com>
2025-10-01 02:55:21 +0800

    Add files via upload
    default版本是素数判断写了p*p <= num的，有一个WA了暂时不知道为什么

---
+++ P1125default.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool prim(int wait)
+{
+    if(wait == 2)   return true;
+    if(wait == 1)   return false;
+    for(int p = 2; p*p <= wait ; p++)
+    {
+        if(wait % p == 0)
+            return false;
+    }
+    return true;
+}
+int main()
+{
+    int cnt[27] = {0};
+    string word;
+    int lns = 0;
+    cin >> word;
+    lns = word.length();
+    for(int i = 0; i < lns; i++)
+    {
+        cnt[word[i]-97]++;
+    }
+    // for(int i = 0; i < 27; i++)
+    // {
+    //     cout << cnt[i];
+    // }测试ASCII程序
+    int low = 114, high = -1;
+    for(int i = 0; i < 26; i++)
+    {
+        if(cnt[i] < low && cnt[i] != 0)    low = cnt[i];
+        if(cnt[i] > high)   high = cnt[i];
+    }
+    if(prim(high - low))
+    {
+        cout << "Lucky Word" << endl << high - low;
+    }
+    else
+        cout << "No Answer" << endl << '0';
+}
