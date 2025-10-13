9dbe9ed55a329eddb2976a54adcf72f60f9cde6c
孟夏雪 <2953904416@qq.com>
2025-10-13 16:48:01 +0800

    Add files via upload
    子集枚举手搓&判断质数
    真傻逼了，应该知道，生成质数和判断现有数是否为质数并不相同，真往日之影了XD
    递归的枚举手法（到时候STL有更方便的手段）
    这种题如果是放在ABC里边应该是剥皮的C题水平（？）
    主要是这些东西更多是纯算法

---
+++ P1036.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n,k;
+int lis[27] = {0};
+int tsum = 0;
+int cnt = 0;
+int slcnt = 0;
+bool isPrime(int wait)
+{   
+    if(wait == 1)   return false;
+    for(int p = 2; p*p <= wait; p++)
+    {
+        if(wait % p == 0 && wait != p)
+            return false;
+    }
+    return true;
+}
+void view(int deps)//目前和？
+{
+    if(slcnt == k)
+    {
+        // cout << tsum << endl;
+        if(isPrime(tsum))   cnt ++;
+        return;
+    }
+    if(deps < n)
+    {
+        tsum += lis[deps];  slcnt ++;
+        view(deps+1);
+        tsum -= lis[deps];  slcnt --;
+        view(deps+1);
+    }
+    return;
+}
+int main()
+{   
+    cin >> n >> k;
+    for(int i = 0; i < n; i++)
+    {
+        cin >> lis[i];
+    }
+    view(0);
+    cout << cnt;
+}
